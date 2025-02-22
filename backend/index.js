a6b68b57ca9253516d861bbb3c564b9ccddf0b94
indresh chauhan <125015046+indreshjack420@users.noreply.github.com>
2025-02-22 10:07:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./socket/socket.js";
 import path from "path";
 import axios from 'axios';
 
-const url = ` http://localhost:5000`;
+const url = `https://socialbanda.onrender.com`;
 const interval = 30000;
 
 function reloadWebsite() {
