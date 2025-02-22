5e6a9dabc429d164b7da8948eb8c4a013df80a40
indresh chauhan <125015046+indreshjack420@users.noreply.github.com>
2025-02-22 10:40:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://socialbanda.onrender.com",
+        target: "http://localhost:5000",
       },
     },
   },
