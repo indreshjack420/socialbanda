f91039fdf93fafd0a9dec1831ee3de610d2f3528
indresh chauhan <125015046+indreshjack420@users.noreply.github.com>
2025-02-22 10:52:00 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://socialbanda.onrender.com",
     methods: ["GET", "POST"],
   },
 });
